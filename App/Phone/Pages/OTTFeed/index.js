9b51bf45f0f21d13cee9b5e7203c71e25fe3a6ad
nazar.kurylo <nazar.kurylo@globallogic.com>
2021-12-14 06:56:53 +0200

    add categories on OTTFeed page

--- App/Phone/Pages/OTTFeed/index.js
+++ App/Phone/Pages/OTTFeed/index.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, TouchableOpacity, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 import { fetchCitySelectionData } from '../../../Core/Stores/CitySelection/Actions';
 import { fetchChannelsData } from '../../../Core/Stores/Channels/Actions';
 import { Header } from '../../Items';
 
+import { styles } from './styles';
 class OTTFeed extends Component {
+  state = {
+    activeCategory: 'All',
+  };
   componentDidMount() {
     const { fetchChannelsData } = this.props;
     fetchChannelsData();
   }
+  handleCategoryPress = (name) => {
+    this.setState({ activeCategory: name });
+  };
   render() {
+    const { activeCategory } = this.state;
     const {
       appStyles: { logo },
       navData,
       station,
       fetchCitySelectionData,
       channelsData,
-      catogories,
+      categories,
     } = this.props;
     return (
       <ScrollView bounces={false} showsVerticalScrollIndicator={false}>
@@ -30,6 +38,46 @@ class OTTFeed extends Component {
           activePage="CHANNELS"
           fetchCitySelectionData={fetchCitySelectionData}
         />
+        {!!channelsData && !!categories && (
+          <View>
+            <ScrollView
+              horizontal
+              bounces={false}
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={styles.categoriesBlock}
+            >
+              <TouchableOpacity
+                style={[
+                  styles.categoryBlock,
+                  activeCategory === 'All' && styles.activeCategoryBlock,
+                ]}
+                onPress={this.handleCategoryPress.bind(this, 'All')}
+              >
+                <Text style={[styles.category, activeCategory === 'All' && styles.activeCategory]}>
+                  All
+                </Text>
+              </TouchableOpacity>
+              {categories.map((category) => {
+                const name = category.name;
+                return (
+                  <TouchableOpacity
+                    style={[
+                      styles.categoryBlock,
+                      activeCategory === name && styles.activeCategoryBlock,
+                    ]}
+                    onPress={this.handleCategoryPress.bind(this, name)}
+                  >
+                    <Text
+                      style={[styles.category, activeCategory === name && styles.activeCategory]}
+                    >
+                      {name}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </ScrollView>
+          </View>
+        )}
       </ScrollView>
     );
   }
@@ -38,12 +86,13 @@ class OTTFeed extends Component {
 const mapStateToProps = ({
   config: { navData, appStyles },
   common: { station },
-  Channels: { channelsData },
+  Channels: { channelsData, categories },
 }) => ({
   navData,
   appStyles,
   station,
   channelsData,
+  categories,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- App/Phone/Pages/OTTFeed/styles.js
+++ App/Phone/Pages/OTTFeed/styles.js
@@ -1,3 +1,32 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  categoriesBlock: {
+    height: 85,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    paddingLeft: 24,
+  },
+  categoryBlock: {
+    backgroundColor: '#DDDDDD30',
+    paddingHorizontal: 4,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: '#DDDDDD30',
+    height: 30,
+    marginRight: 24,
+    justifyContent: 'center',
+  },
+  activeCategoryBlock: {
+    backgroundColor: undefined,
+    borderColor: '#FFDA3A',
+  },
+  category: {
+    color: '#ffffff',
+    fontSize: 17,
+    fontFamily: 'Montserrat-Semibold',
+  },
+  activeCategory: {
+    color: '#FFDA3A',
+  },
+});
